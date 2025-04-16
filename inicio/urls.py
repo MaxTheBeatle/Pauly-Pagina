cc44c296ccd0ffeb4f220b6e6ab0f97ddf57c1b0
MaxTheBeatle <65696291+MaxTheBeatle@users.noreply.github.com>
2025-04-16 00:41:24 -0400

    a

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.inicio_view, name='inicio'),
+    path('', include('paulina.urls')), 
 ]
