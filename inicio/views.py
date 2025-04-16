a480c9d5389531f965880e66bd51bd51fd06d1e1
MaxTheBeatle <65696291+MaxTheBeatle@users.noreply.github.com>
2025-04-16 00:37:16 -0400

    a

--- inicio/views.py
+++ inicio/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def inicio_view(request):
     return render(request, 'inicio.html')
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("¡Hola, bienvenido a nuestra página web!")
